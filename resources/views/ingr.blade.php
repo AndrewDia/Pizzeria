1ffcb5df7cc649a3a33547008f0922e496acfeeb
Олексій <oleksijkopija@gmail.com>
2021-05-21 19:18:46 +0300

    Custom pizza ordering

--- resources/views/ingr.blade.php
+++ resources/views/ingr.blade.php
@@ -3,9 +3,9 @@
     <h1>{{$ingr->name}}</h1>
     <h2>{{$ingr->price}} грн / {{$ingr->weight}} г</h2>
     <div class="ingredient_counter">
-            <p class="increase">+</p>
-            <input form="create_form" id="ingr_{{$ingr->id}}" name="ingr_{{$ingr->id}}" class="black_square" value="0"
-                   type="text" readonly>
-            <p class="decrease">&ndash;</p>
+        <p class="increase">+</p>
+        <input form="create_form" id="ingr_{{$ingr->id}}" name="ingr_{{$ingr->id}}" class="black_square"
+               value="{{$add[$ingr->id] ?? 0}}" type="text" readonly>
+        <p class="decrease">&ndash;</p>
     </div>
 </div>
